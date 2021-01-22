2fe201d11df7ef2e9b5df502d170c9de5f0437b8
David Yeung <quxbaz@gmail.com>
2021-01-21 19:57:01 -0700

    Update.

--- src/reports-charts/ReportsLineChart.js
+++ src/reports-charts/ReportsLineChart.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Card} from '@shopify/polaris'
-import {ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Line, Label} from 'recharts'
+import {ResponsiveContainer, LineChart, CartesianGrid, CartesianAxis, XAxis, YAxis, Tooltip, Line, Label} from 'recharts'
 import colors from './colors'
 import {formatTick} from './util'
 import CustomLegend from './CustomLegend'
@@ -8,12 +8,8 @@ import CustomLegend from './CustomLegend'
 //
 import {generateNormalReports} from './debug'
 
-const LINE_PROPS = {type: 'monotone', dot: false, strokeWidth: 2.5}
+const LINE_PROPS = {type: 'linear', dot: false, strokeWidth: 2}
 const DATA = generateNormalReports()
-
-const LabelY = () => (
-  <span>foobar</span>
-)
 
 function ReportsLineChart () {
   return (
@@ -21,12 +17,12 @@ function ReportsLineChart () {
       <Card.Section title="Last 60 days">
         <ResponsiveContainer height={240}>
           <LineChart data={DATA} margin={{top: 0, right: 0, bottom: 0, left: 0}}>
-            <CartesianGrid stroke='hsl(0, 0%, 92%)' />
+            <CartesianGrid stroke={colors.GRID_LINE} />
             <XAxis dataKey="date"
                    axisLine={{stroke: colors.AXIS_LINE, strokeWidth: 1}}
                    tick={{fill: colors.TICK}} tickSize={12} tickMargin={4}
                    tickLine={{stroke: colors.TICK_LINE}} minTickGap={30} tickFormatter={formatTick} />
-            <YAxis mirror label={LabelY} />
+            <YAxis mirror />
             <Tooltip isAnimationActive={false} />
             <Line {...LINE_PROPS} name='Sent'    dataKey='quantity_sent'    stroke={colors.SENT} />
             <Line {...LINE_PROPS} name='Pending' dataKey='quantity_pending' stroke={colors.PENDING} />
