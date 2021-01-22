04d1daf0355983de8932a7a85d3b3eff021e0b04
David Yeung <quxbaz@gmail.com>
2021-01-21 19:56:41 -0700

    Update colors.

--- src/reports-charts/colors.js
+++ src/reports-charts/colors.js
@@ -1,13 +1,10 @@
 const colors = {
-  // SENT: 'hsl(212, 100%, 60%)',
-  // PENDING: '#f5b840',
-  // FAILED: '#f15947',
   SENT: 'hsl(256deg 60% 67%)',
   PENDING: 'hsl(206deg 47% 70%)',
-  FAILED: 'hsl(3deg 76% 65%)',
-  GRID_LINE: 'hsl(0deg 0% 92%)',
-  AXIS_LINE: 'hsl(0deg 0% 68%)',
-  TICK_LINE: 'hsl(0deg 0% 68%)',
+  FAILED: 'hsl(3deg 76% 69%)',
+  GRID_LINE: 'hsl(198deg 37% 88%)',
+  AXIS_LINE: 'hsl(198deg 37% 88%)',
+  TICK_LINE: 'hsl(198deg 37% 88%)',
   TICK: 'hsl(0deg 0% 60%)',
 }
 
