e16efe3a615311901b21b34e7cc61f2a1e765f9d
David Yeung <quxbaz@gmail.com>
2021-01-27 11:41:56 -0700

    Render need more data.

---
+++ src/reports-charts/NotEnoughData.js
@@ -0,0 +1,12 @@
+import styles from './NotEnoughData.module.css'
+import React from 'react'
+
+const NotEnoughData = () => (
+  <div className={styles.NotEnoughData}>
+    <div className={styles.Text}>
+      Need more data to render chart.
+    </div>
+  </div>
+)
+
+export default NotEnoughData
--- src/reports-charts/ReportsLineChart.js
+++ src/reports-charts/ReportsLineChart.js
@@ -4,6 +4,7 @@ import {Card} from '@shopify/polaris'
 import {ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Line} from 'recharts'
 import colors from './colors'
 import {formatDate, formatTick} from './util'
+import NotEnoughData from './NotEnoughData'
 import CustomLegend from './CustomLegend'
 
 //
@@ -16,24 +17,27 @@ function ReportsLineChart () {
   return (
     <Card>
       <Card.Section title="SMS Messages Sent in the Last 60 Days">
-        <ResponsiveContainer className={chartStyles.Container} height={240}>
-          <LineChart data={DATA} margin={{left: -30, right: 0}}>
-            <CartesianGrid stroke={colors.GRID_LINE} />
-            <XAxis dataKey="date"
-                   axisLine={{stroke: colors.AXIS_LINE, strokeWidth: 1}}
-                   tick={{fontSize: 12, fill: colors.TICK}} tickSize={12} tickMargin={4}
-                   tickLine={{stroke: colors.TICK_LINE}} minTickGap={30} tickFormatter={formatTick} />
-            <Tooltip isAnimationActive={false} labelFormatter={formatDate} />
-            <Line {...LINE_PROPS} name="Sent"    dataKey="quantity_sent"    stroke={colors.SENT} />
-            <Line {...LINE_PROPS} name="Pending" dataKey="quantity_pending" stroke={colors.PENDING} />
-            <Line {...LINE_PROPS} name="Failed"  dataKey="quantity_failed"  stroke={colors.FAILED} />
-            <YAxis dataKey="quantity_sent"
-                   axisLine={false} tickLine={false}
-                   tickLine={{stroke: colors.TICK_LINE}}
-                   tickSize={12} tickMargin={4}
-                   tick={{fontSize: 12, fill: colors.TICK}} />
-          </LineChart>
-        </ResponsiveContainer>
+        <div className={chartStyles.TopContainer}>
+          <ResponsiveContainer className={chartStyles.Container} height={240}>
+            <LineChart data={DATA} margin={{left: -30, right: 0}}>
+              <CartesianGrid stroke={colors.GRID_LINE} />
+              <XAxis dataKey="date"
+                     axisLine={{stroke: colors.AXIS_LINE, strokeWidth: 1}}
+                     tick={{fontSize: 12, fill: colors.TICK}} tickSize={12} tickMargin={4}
+                     tickLine={{stroke: colors.TICK_LINE}} minTickGap={30} tickFormatter={formatTick} />
+              <Tooltip isAnimationActive={false} labelFormatter={formatDate} />
+              <Line {...LINE_PROPS} name="Sent"    dataKey="quantity_sent"    stroke={colors.SENT} />
+              <Line {...LINE_PROPS} name="Pending" dataKey="quantity_pending" stroke={colors.PENDING} />
+              <Line {...LINE_PROPS} name="Failed"  dataKey="quantity_failed"  stroke={colors.FAILED} />
+              <YAxis dataKey="quantity_sent"
+                     axisLine={false} tickLine={false}
+                     tickLine={{stroke: colors.TICK_LINE}}
+                     tickSize={12} tickMargin={4}
+                     tick={{fontSize: 12, fill: colors.TICK}} />
+            </LineChart>
+          </ResponsiveContainer>
+          <NotEnoughData />
+        </div>
         <CustomLegend />
       </Card.Section>
     </Card>
